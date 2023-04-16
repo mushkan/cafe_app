43c7cc9d05a48f6dea7b947df6a7dcd7858ef3f5
mushkan goyal <mushkangoyal2299@gmail.com>
2023-04-16 23:31:33 +0530

    initial commit

---
+++ src/main/java/com/cafe_app/CentralCafeAppApplication.java
@@ -0,0 +1,13 @@
+package com.cafe_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralCafeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralCafeAppApplication.class, args);
+	}
+
+}
